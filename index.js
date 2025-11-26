8212ed8a1bf3b7f09ac2071f9bb1d823a27c2d49
oliullahakib <oliullahakib@gmail.com>
2025-11-26 13:13:28 +0600

    added api for user and rider

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ app.get('/', (req, res) => {
   res.send('zap is shifting!!')
 })
 
+// traking id generator 
+function createTrackingId() {
+  const prefix = 'BDX-';
+  const timestampPart = Date.now().toString(36).toUpperCase();
+  const randomPart = Math.random().toString(16).substring(2, 8).toUpperCase();
+  return `${prefix}${timestampPart}-${randomPart}`;
+}
 
 async function run() {
   try {
@@ -33,7 +40,20 @@ async function run() {
     await client.connect();
     const db = client.db("Zap-Shift")
     const parcelCollection = db.collection("parcels")
+    const paymentCollection = db.collection("payment")
+    const usersCollection = db.collection("users")
+    const riderCollection = db.collection("rider")
 
+    // user releted apis 
+    app.post('/user', async (req, res) => {
+      const newUser = req.body
+      newUser.createdAt = new Date()
+      newUser.role = "user"
+      const result = await usersCollection.insertOne(newUser)
+      res.send(result)
+    })
+
+    // parcel releted apis 
     app.get('/my-parcels', async (req, res) => {
       const { email } = req.query
       const query = {}
@@ -59,30 +79,100 @@ async function run() {
     // payment related apis 
 
     app.post('/payment-checkout-session', async (req, res) => {
-      const parcelInfo=req.body
-      const{email,name,productId,cost} = parcelInfo
-      const amount= parseInt(cost)*100
+      const parcelInfo = req.body
+      const { email, name, parcelId, cost } = parcelInfo
+      const amount = parseInt(cost) * 100
       const session = await stripe.checkout.sessions.create({
         line_items: [
           {
-           price_data:{
-            currency:'USD',
-            product_data:{name:name},
-            unit_amount:amount
-           },
+            price_data: {
+              currency: 'USD',
+              product_data: { name: name },
+              unit_amount: amount
+            },
             quantity: 1,
           },
         ],
         mode: 'payment',
-        customer_email:email,
-        metadata:{
-          productId:productId
+        customer_email: email,
+        metadata: {
+          parcelId: parcelId
         },
-        success_url: `${process.env.DOMAIN_NAME}/dashboard/payment-success`,
+        success_url: `${process.env.DOMAIN_NAME}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.DOMAIN_NAME}/dashboard/payment-cancel`,
       })
-      res.send({url:session.url})
+      res.send({ url: session.url })
     })
+
+    app.patch('/payment', async (req, res) => {
+      const { session_id } = req.query
+      const sessonData = await stripe.checkout.sessions.retrieve(session_id)
+      const trakingId = createTrackingId()
+      // modify the parcel 
+      const id = sessonData.metadata.parcelId
+      const filter = { id: new ObjectId(id) }
+      const update = {
+        $set: {
+          paymentStatus: sessonData.payment_status,
+          trakingId
+        }
+      }
+      const modifyResult = parcelCollection.updateOne(filter, update)
+
+      // creat a transaciton history 
+      const paymentInfo = {
+        amoun: sessonData.amount_total,
+        currency: sessonData.currency,
+        payer_email: sessonData.customer_email,
+        productId: sessonData.metadata.parcelId,
+        transactionId: sessonData.payment_intent,
+        paymentStatus: sessonData.payment_status,
+        payAt: new Date()
+
+      }
+      const query = { transactionId: paymentInfo.transactionId }
+      const parcelExsit = await paymentCollection.findOne(query)
+
+      if (parcelExsit) {
+        return res.send({
+          message: "Payment already complite for this parcel",
+          transactionId: sessonData.payment_intent,
+          trakingId,
+        })
+      }
+      const paymentResult = paymentCollection.insertOne(paymentInfo)
+
+      res.send({
+        message: "True",
+        paymentResult,
+        transactionId: sessonData.payment_intent,
+        trakingId,
+        modifyResult
+      })
+    })
+
+    // rider related apis 
+    app.post('/rider', async (req, res) => {
+      const newRider = req.body
+      newRider.createdAt = new Date()
+      newRider.status = "pending"
+      if(!newRider.email){
+        return res.send({message:"Email is requerd to apply"})
+      }
+      // checking the application 
+      console.log(newRider.email)
+      const query = {}
+      if (newRider.email) {
+        query.email = newRider.email
+      }
+      const riderExist = riderCollection.findOne(query)
+      if (riderExist) {
+        return res.send({ message: "Your Application already taken.Please wait for approval" })
+      }
+      const result = await riderCollection.insertOne(newRider)
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
