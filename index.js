4db46ac4c69f7b40286ba86b93c8877f17f8bb16
oliullahakib <oliullahakib@gmail.com>
2025-11-28 17:14:20 +0600

    fix a bix bug for tracking id and adde delivery status for success payment

--- index.js
+++ index.js
@@ -20,32 +20,31 @@ const client = new MongoClient(uri, {
   }
 });
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount)
 });
 // midleware 
 app.use(cors())
 app.use(express.json())
 
 // cusmont middleware 
-const vrifyFriebaseToken=async(req,res,next)=>{
+const vrifyFriebaseToken = async (req, res, next) => {
   const authorization = req.headers.authorization
-  
-  if(!authorization){
-    return res.status(401).send({message:"unauthorize access"})
+
+  if (!authorization) {
+    return res.status(401).send({ message: "unauthorize access" })
   }
   const token = authorization.split(' ')[1]
- 
-  if(!token){
-     return res.status(401).send({message:"unauthorize access"})
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorize access" })
   }
   try {
-   console.log(token)
-  const decoded = await admin.auth().verifyIdToken(token)
-  req.token_email = decoded.email
-  next()
- } catch (error) {
-  return res.status(401).send({message:"unauthorize access"})
- }
+    const decoded = await admin.auth().verifyIdToken(token)
+    req.token_email = decoded.email
+    next()
+  } catch (error) {
+    return res.status(401).send({ message: "unauthorize access" })
+  }
 }
 
 app.get('/', (req, res) => {
@@ -72,44 +71,44 @@ async function run() {
 
     // middleware with db 
 
-    const verifyAdmin = async(req,res,next)=>{
-    const email = req.token_email
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.token_email
       const query = {}
-      if(email){
+      if (email) {
         query.email = email
       }
       const result = await usersCollection.findOne(query)
-      if(result.role!=="admin"){
-        return res.status(403).send({message:"Forbidden access"})
+      if (result.role !== "admin") {
+        return res.status(403).send({ message: "Forbidden access" })
       }
       next()
     }
 
     // user releted apis 
-    app.get('/users',vrifyFriebaseToken,verifyAdmin,async(req,res)=>{
+    app.get('/users', vrifyFriebaseToken, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray()
       res.send(result)
     })
-    app.get('/user/:email/role',async(req,res)=>{
-      const {email} = req.params
+    app.get('/user/:email/role', async (req, res) => {
+      const { email } = req.params
       const query = {}
-      if(email){
+      if (email) {
         query.email = email
       }
       const result = await usersCollection.findOne(query)
-      res.send({role:result.role})
+      res.send({ role: result.role })
     })
-    app.patch('/user/:id/role',vrifyFriebaseToken,async(req,res)=>{
-      const {id} = req.params
-      const {role} = req.body
+    app.patch('/user/:id/role', vrifyFriebaseToken, async (req, res) => {
+      const { id } = req.params
+      const { role } = req.body
       const query = {}
-      if(id){
-        query._id=new ObjectId(id)
+      if (id) {
+        query._id = new ObjectId(id)
       }
       const update = {
-        $set:{role}
+        $set: { role }
       }
-      const result = await usersCollection.updateOne(query,update)
+      const result = await usersCollection.updateOne(query, update)
       res.send(result)
     })
     app.post('/user', async (req, res) => {
@@ -121,7 +120,7 @@ async function run() {
     })
 
     // parcel releted apis 
-    app.get('/my-parcels', async (req, res) => {
+    app.get('/my-parcels', vrifyFriebaseToken, async (req, res) => {
       const { email } = req.query
       const query = {}
       if (email) {
@@ -133,6 +132,9 @@ async function run() {
     app.post('/parcels', async (req, res) => {
       const newParcel = req.body
       newParcel.createdAt = new Date()
+      // need to add tracking id here only 
+      const trakingId = createTrackingId()
+      newParcel.trakingId = trakingId;
       const result = await parcelCollection.insertOne(newParcel)
       res.send(result)
     })
@@ -163,7 +165,8 @@ async function run() {
         mode: 'payment',
         customer_email: email,
         metadata: {
-          parcelId: parcelId
+          parcelId: parcelId,
+          trakingId:parcelInfo.trakingId
         },
         success_url: `${process.env.DOMAIN_NAME}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.DOMAIN_NAME}/dashboard/payment-cancel`,
@@ -171,20 +174,33 @@ async function run() {
       res.send({ url: session.url })
     })
 
-    app.patch('/payment', async (req, res) => {
+    app.patch('/payment-success', async (req, res) => {
       const { session_id } = req.query
       const sessonData = await stripe.checkout.sessions.retrieve(session_id)
-      const trakingId = createTrackingId()
+
+
+      // check the parcle first 
+      const query = { transactionId: sessonData.payment_intent }
+      const parcelExsit = await paymentCollection.findOne(query)
+      if (parcelExsit) {
+        return res.send({
+          message: "Payment already complite for this parcel",
+          transactionId: sessonData.payment_intent,
+          trakingId:parcelExsit.trakingId,
+        })
+      }
       // modify the parcel 
       const id = sessonData.metadata.parcelId
-      const filter = { id: new ObjectId(id) }
+      const trakingId = sessonData.metadata.trakingId;
+      const filter = { _id: new ObjectId(id) }
       const update = {
         $set: {
           paymentStatus: sessonData.payment_status,
-          trakingId
+          trakingId,
+          deliveryStatus: "pending-pickup"
         }
       }
-      const modifyResult = parcelCollection.updateOne(filter, update)
+      const modifyResult = await parcelCollection.updateOne(filter, update)
 
       // creat a transaciton history 
       const paymentInfo = {
@@ -194,23 +210,13 @@ async function run() {
         productId: sessonData.metadata.parcelId,
         transactionId: sessonData.payment_intent,
         paymentStatus: sessonData.payment_status,
-        payAt: new Date()
-
+        payAt: new Date(),
+        trakingId
       }
-      const query = { transactionId: paymentInfo.transactionId }
-      const parcelExsit = await paymentCollection.findOne(query)
-
-      if (parcelExsit) {
-        return res.send({
-          message: "Payment already complite for this parcel",
-          transactionId: sessonData.payment_intent,
-          trakingId,
-        })
-      }
-      const paymentResult = paymentCollection.insertOne(paymentInfo)
+      const paymentResult = await paymentCollection.insertOne(paymentInfo)
 
       res.send({
-        message: "True",
+        message: "success",
         paymentResult,
         transactionId: sessonData.payment_intent,
         trakingId,
@@ -220,16 +226,16 @@ async function run() {
 
     // rider related apis 
 
-    app.get('/riders',vrifyFriebaseToken,verifyAdmin,async(req,res)=>{
-      const result = await riderCollection.find().sort({createdAt:-1}).toArray()
+    app.get('/riders', vrifyFriebaseToken, verifyAdmin, async (req, res) => {
+      const result = await riderCollection.find().sort({ createdAt: -1 }).toArray()
       res.send(result)
     })
-    app.post('/rider', vrifyFriebaseToken,async (req, res) => {
+    app.post('/rider', vrifyFriebaseToken, async (req, res) => {
       const newRider = req.body
       newRider.createdAt = new Date()
       newRider.status = "pending"
-      if(!newRider.email){
-        return res.send({message:"Email is requerd to apply"})
+      if (!newRider.email) {
+        return res.send({ message: "Email is requerd to apply" })
       }
       // checking the application 
       const query = {}
@@ -243,32 +249,32 @@ async function run() {
       const result = await riderCollection.insertOne(newRider)
       res.send(result)
     })
-   
-     app.delete('/rider/:id',vrifyFriebaseToken,verifyAdmin, async (req, res) => {
+
+    app.delete('/rider/:id', vrifyFriebaseToken, verifyAdmin, async (req, res) => {
       const { id } = req.params
       const query = { _id: new ObjectId(id) }
       const result = await riderCollection.deleteOne(query)
       res.send(result)
     })
-     app.patch('/rider/:id',vrifyFriebaseToken,verifyAdmin,async(req,res)=>{
-       const { id } = req.params
-       console.log(req.body)
-       const {status,email} = req.body
+    app.patch('/rider/:id', vrifyFriebaseToken, verifyAdmin, async (req, res) => {
+      const { id } = req.params
+      console.log(req.body)
+      const { status, email } = req.body
       const query = { _id: new ObjectId(id) }
       const update = {
-        $set:{status}
+        $set: { status }
       }
-      const result = await riderCollection.updateOne(query,update)
+      const result = await riderCollection.updateOne(query, update)
 
-      if(status==="accepted"){
-        const query={}
-        if(email){
-          query.email=email
+      if (status === "accepted") {
+        const query = {}
+        if (email) {
+          query.email = email
         }
         const updateRole = {
-          $set:{role:"rider"}
+          $set: { role: "rider" }
         }
-        const userResult = usersCollection.updateOne(query,updateRole)
+        const userResult = usersCollection.updateOne(query, updateRole)
       }
       res.send(result)
     })
